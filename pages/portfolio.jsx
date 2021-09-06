17336fcf6a9f4fd476cc1950617d2913adcfe768
Felix Lang <felix.lang@mailbox.org>
2021-09-06 17:32:10 +0200

    Revome portfolio placeholders.

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -3,41 +3,24 @@ import Link from 'next/link';
 import Navbar from '../components/Navbar';
 
 function Portfolio() {
-	return (
-		<>
-			<Head>
-			  <title>Portfolio</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
 
-			<Navbar />
+      <Navbar />
 
-			<section className="hero">
-			  <div className="container">
-			    <div className="text-wrapper w-full">
-			      <h1 className="title">Portfolio</h1>
-			      <p className="description">
-			      	Nothing to be seen here yet.
-			      </p>
-
-			      <div className="portfolio-wrapper">
-			      	<div className="portfolio-item">
-			      		<img src="/chitchat.png" className="portfolio-image" />
-
-			      		<h4 className="portfolio-name">Chat App Exploration</h4>
-			      		<div className="portfolio-category">Mobile Dev</div>
-			      	</div>
-			      	<div className="portfolio-item">
-			      		<img src="/kajian.jpg" className="portfolio-image" />
-
-			      		<h4 className="portfolio-name">kajian App</h4>
-			      		<div className="portfolio-category">Mobile Dev</div>
-			      	</div>
-			      </div>
-		      </div>
-	      </div>
-			</section>
-		</>
-	);
+      <section className="hero">
+        <div className="container">
+          <div className="text-wrapper w-full">
+            <h1 className="title">Portfolio</h1>
+            <p className="description">Nothing to be seen here yet.</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Portfolio;
